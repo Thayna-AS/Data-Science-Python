b4b3c38a17df6e88c34334245fa50b6b0303e8b6
Thayna-AS <thayna.almeida.silva23@gmail.com>
2024-08-18 20:58:47 -0300

    Iniciando meus estudos com Python

---
+++ helloWorld.py
@@ -0,0 +1,15 @@
+print('Hello World!')
+
+print('Nome: Thayna')
+print('Sobrenome: Silva')
+
+print('T')
+print('h')
+print('a')
+print('y')
+print('n')
+print('a')
+
+print(11, 'Junho', 1999)
+
+print('Ano atual:', 2024)
